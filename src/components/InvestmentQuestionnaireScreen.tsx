c21d3ebb6f303c1a56a719df364ce6f5c7b5dba7
Juxhin Xhihani <63799887+juxhinxhihani@users.noreply.github.com>
2025-07-17 09:51:50 +0200

    Fix PostCSS configuration conflict

--- src/components/InvestmentQuestionnaireScreen.tsx
+++ src/components/InvestmentQuestionnaireScreen.tsx
@@ -352,6 +352,7 @@ export default function InvestmentQuestionnaireScreen({
                   </tbody>
                 </table>
               </div>
+            </div>
           )}
         </div>
 

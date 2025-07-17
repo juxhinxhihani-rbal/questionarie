856476d14dbe73b2f42747e15ca5d9ba731ff3f7
juxhinxhihani-rbal <juxhin.xhihani@raiffeisen.al>
2025-07-17 10:57:57 +0200

    Setup Debug Mode for Next.js Application

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Enable source maps in development
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.devtool = 'eval-source-map';
+    }
+    return config;
+  },
   images: {
     domains: ["images.unsplash.com"],
   },
